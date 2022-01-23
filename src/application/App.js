a0f925512f92e583eba2d3d11dfc7be4bcda257c
jordi <jordi.vilagut@andjoy.life>
2022-01-23 18:53:16 +0100

    Start personal website project

---
+++ src/application/App.js
@@ -0,0 +1,10 @@
+import '../styles/App.css';
+import '../components/Navbar'
+import {Navbar} from "../components/Navbar";
+
+export const App = () => (
+    <div className="App">
+        <Navbar/>
+        <h1>Hi, I'm Jordi Vilagut</h1>
+    </div>
+)
---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export const Navbar = () => (
+    <div id="navBar">
+        <h1>Jordi Vilagut</h1>
+    </div>
+);
